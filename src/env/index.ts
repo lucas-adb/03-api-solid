b057d7c45ebda4b6567576665e1f7da1cab9f01d
lucas-adb <alvesdebrito.lucas@gmail.com>
2025-07-13 15:39:17 -0300

    feat: adds env files and changes lib versions to match the course lessons

---
+++ src/env/index.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const errorMessages = {
+  INVALID_ENV: '❌ Invalid environment variables',
+};
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error(errorMessages.INVALID_ENV, _env.error.format());
+
+  throw new Error(errorMessages.INVALID_ENV);
+}
+
+export const env = _env.data;
