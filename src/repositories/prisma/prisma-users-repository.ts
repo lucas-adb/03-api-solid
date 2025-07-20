3701b9245d41b9dbc8c3b1374d50fe5efd626f36
lucas-adb <alvesdebrito.lucas@gmail.com>
2025-07-20 11:42:52 -0300

    feat: adds prisma check in and user repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,15 +1,11 @@
 import { prisma } from '@/lib/prisma'
-import { Prisma, User } from '@prisma/client'
+import { Prisma } from '@prisma/client'
 import { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
-  async findById(id: string): Promise<User | null> {
-    throw new Error('Method not implemented')
-  }
-
-  async create(data: Prisma.UserCreateInput) {
-    const user = await prisma.user.create({
-      data,
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: { id },
     })
 
     return user
@@ -18,6 +14,14 @@ export class PrismaUsersRepository implements UsersRepository {
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: { email },
+    })
+
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
     })
 
     return user
