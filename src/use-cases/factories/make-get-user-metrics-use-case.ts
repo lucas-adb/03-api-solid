32db6f0e9ae48af3a03b1e19e5ea281c6da7e280
lucas-adb <alvesdebrito.lucas@gmail.com>
2025-07-21 11:05:42 -0300

    feat: adds all factories

---
+++ src/use-cases/factories/make-get-user-metrics-use-case.ts
@@ -0,0 +1,9 @@
+import { GetUsersMetricsUseCase } from '../get-user-metrics'
+import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
+
+export function makeGetUserMetricsUseCase() {
+  const checkInsRepository = new PrismaCheckInsRepository()
+  const useCase = new GetUsersMetricsUseCase(checkInsRepository)
+
+  return useCase
+}
